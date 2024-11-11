cbdbd1cafed95673ce4ff3a3141948969f74cb08
EC2 Default User <ec2-user@ip-172-31-22-170.ec2.internal>
2024-11-11 11:08:25 +0000

    terraform files

---
+++ terraform/variable.tf
@@ -0,0 +1,10 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
