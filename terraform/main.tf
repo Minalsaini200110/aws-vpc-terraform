c12b637535af8d57bc030b6f262ecbae2e4c6777
EC2 Default User <ec2-user@ip-172-31-90-249.ec2.internal>
2024-11-12 07:19:17 +0000

    change in cidr

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 # Create VPC
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "10.1.0.0/16"
   enable_dns_support = true
   enable_dns_hostnames = true
 }
