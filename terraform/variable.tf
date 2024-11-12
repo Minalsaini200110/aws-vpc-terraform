c3ed490714e21347d81b19460940493813303121
Minalsaini200110 <minalsaini1003@gmail.com>
2024-11-12 13:11:35 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,10 +1,9 @@
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  default     = "10.0.0.0/16"
-}
-
 variable "region" {
-  description = "AWS region"
-  default     = "us-east-1"
+ default = "us-east-1"
 }
-
+variable "vpc_cidr_block" {
+ default = "10.0.0.0/16"
+}
+variable "public_subnet_cidr" {
+ default = "10.0.1.0/24"
+}
