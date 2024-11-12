1009652d910498612c622ab6ea663bf81803ad02
Minalsaini200110 <minalsaini1003@gmail.com>
2024-11-12 13:10:44 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,47 +1,39 @@
 provider "aws" {
-  region = "us-east-1"
+ region = "us-east-1"
 }
-
-# Create VPC
-resource "aws_vpc" "main" {
-  cidr_block = "10.1.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostnames = true
+resource "aws_vpc" "my_vpc" {
+ cidr_block = "10.0.0.0/16"
+ tags = {
+ Name = "MyVPC"
+ }
 }
-
-# Public Subnet
-resource "aws_subnet" "public" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-  map_public_ip_on_launch = true
+resource "aws_subnet" "public_subnet" {
+ vpc_id = aws_vpc.my_vpc.id
+ cidr_block = "10.0.1.0/24"
+ map_public_ip_on_launch = true
+ availability_zone = "us-east-1a"
+ tags = {
+ Name = "PublicSubnet"
+ }
 }
-
-# Private Subnet
-resource "aws_subnet" "private" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
-  availability_zone = "us-east-1a"
+resource "aws_internet_gateway" "my_igw" {
+ 
+ vpc_id = aws_vpc.my_vpc.id
+ tags = {
+ Name = "MyInternetGateway"
+ }
 }
-
-# Internet Gateway
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
+resource "aws_route_table" "public_route_table" {
+ vpc_id = aws_vpc.my_vpc.id
+ route {
+ cidr_block = "0.0.0.0/0"
+ gateway_id = aws_internet_gateway.my_igw.id
+ }
+ tags = {
+ Name = "PublicRouteTable"
+ }
 }
-
-# Route Table for Public Subnet
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
+resource "aws_route_table_association" "public_subnet_association" {
+ subnet_id = aws_subnet.public_subnet.id
+route_table_id = aws_route_table.public_route_table.id
 }
-
-resource "aws_route" "internet_access" {
-  route_table_id         = aws_route_table.public.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.gw.id
-}
-
-resource "aws_route_table_association" "public_association" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
-}
-
